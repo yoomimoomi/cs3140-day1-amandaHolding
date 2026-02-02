ea0dbbda14d34cc3d6017e720d3367697de19991
ash <ash@THEMIS.localdomain>
2026-02-01 21:37:29 -0500

    updated avg min and sum functions

---
+++ index.js
@@ -0,0 +1,38 @@
+
+// ARRAYS 
+
+let arr = [1, 2, 3, 4, 5];
+
+function sum(arr) {
+    let sum = 0;
+    for (const num of arr) {
+        sum += num;
+    }
+    return sum;
+}
+
+function average (arr) {
+    let sum = 0;
+    for (const num of arr) {
+        sum += num;
+    }
+    return sum / arr.length;
+}
+
+function min(arr) {
+    let min = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    return min;
+    }
+}
+
+function min(arr) {
+    
+}
+
+// STRINGS
+
+
